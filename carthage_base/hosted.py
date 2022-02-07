1c0d4e307792e827efd01df3925343d3ea691912
Sam Hartman <sam.hartman@hadronindustries.com>
2022-02-07 11:51:00 -0700

    Update hosted
    Track where machines are running if they are running to avoid
    duplicates on shared storage.  Provide more flexibility in configuring
    where to start machines.

--- carthage_base/hosted.py
+++ carthage_base/hosted.py
@@ -9,6 +9,7 @@
 import fcntl
 import os
 import socket
+import types
 from carthage import *
 import carthage.container
 import carthage.vm
@@ -32,7 +33,7 @@ def _checkhost(model, file=None):
         if close: file.close()
 
 def _sethost(model, host):
-    fd = os.open(model.stamp_path/".host", os.O_CREAT|os.O_RDWR, 0o664)
+    fd = os.open(model.stamp_path/".host", os.O_CREAT|os.O_RDWR|os.O_CLOEXEC, 0o664)
     with open(fd, "r+t") as file:
         fcntl.lockf(fd,fcntl.LOCK_EX)
         cur_host = _checkhost(model, file=file)
@@ -42,25 +43,59 @@ def _sethost(model, host):
         file.seek(0)
         file.write(host)
 
+def clear_hosted(model):
+    "Clear indication that a model's implementation is running somewhere.  The caller must guarantee the model is not running otherwise the model may be started in two locations."
+    try: os.unlink(model.stamp_path/'.host')
+    except FileNotFoundError: pass
+
+__all__ += ['clear_hosted']
+
 class RunningElsewhere(RuntimeError): pass
+
 __all__ += ['RunningElsewhere']
+
 
             
 @inject(host = InjectionKey("host"),
+        model = InjectionKey(AbstractMachineModel, _optional=True, _ready=False),
         injector=Injector
         )
 class HostedMachine(Injectable):
 
-    def __new__(cls, *, host, injector):
+    def __new__(cls, *, host, model, injector):
         config = injector.get_instance(ConfigLayout)
-        if cls.is_locally_hosted(host,config):
+        if cls.is_locally_hosted(host,config, model):
             return cls.implementation
         return BareMetalMachine
 
     @classmethod
-    def is_locally_hosted(cls, host, config):
-        if config.locally_hosted: return host in config.locally_hosted
-        return host == socket.getfqdn()
+    def is_locally_hosted(cls, host, config, model):
+        if model and getattr(model, 'force_locally_hosted', False): return True
+        if model:
+            cur_host = _checkhost(model)
+            if cur_host: return cur_host == socket.getfqdn()
+        if config.locally_hosted and  host in config.locally_hosted: return True
+        if (not config.locally_hosted) or 'localhost' in config.locally_hosted:
+            return host == socket.getfqdn()
+        return False
+
+    def __init_subclass__(cls, *args, **kwargs):
+        super().__init_subclass__(*args, **kwargs)
+        if not issubclass(cls.implementation, HostedTrackerMixin):
+            cls.implementation = types.new_class(
+                cls.implementation.__qualname__,
+                (cls.implementation, HostedTrackerMixin))
+
+class HostedTrackerMixin(Machine):
+    async def start_machine(self, **kwargs):
+        _sethost(self.model, socket.getfqdn())
+        return await super().start_machine(**kwargs)
+
+    async def stop_machine(self):
+        clear_hosted(self.model)
+        return await super().stop_machine()
+
+__all__ += ['HostedTrackerMixin']
 
 class HostedContainer(HostedMachine):
     implementation = carthage.container.Container
@@ -69,11 +104,11 @@ class HostedVm(HostedMachine):
     implementation = carthage.vm.Vm
 
 @inject(host=None,
-        model=AbstractMachineModel)
+        model=InjectionKey(AbstractMachineModel, _ready=False))
 class BareOrLocal(HostedMachine):
     implementation = carthage.local.LocalMachine
 
     def __new__(cls, *, injector, model, **kwargs):
-        return super().__new__(cls, injector=injector, host=model.name, **kwargs)
+        return super().__new__(cls, injector=injector, host=model.name, model=model, **kwargs)
 
 __all__ += ['HostedContainer', 'HostedVm', 'BareOrLocal']
